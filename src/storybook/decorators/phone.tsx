2327e3e9053bfca4ed164af06028be6e624925fd
Agustin Allamano Costa <allamanocostaagustin@gmail.com>
2023-11-24 19:31:18 +1100

    test: 💍 add snapshot testing

--- src/storybook/decorators/phone.tsx
+++ src/storybook/decorators/phone.tsx
@@ -1,14 +1,24 @@
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { styled } from "styled-components";
 import credentials from "../../assets/google/client_secret_153375467669-bl3575q27tmq97cf359v0unoegk06opn.apps.googleusercontent.com.json";
+import { Children } from "react";
 
 export const PhoneContext = (Story: any) => (
-  <GoogleOAuthProvider clientId={credentials.web.client_id}>
-    <MyPhoneContext>
-      <Story />
-    </MyPhoneContext>
-  </GoogleOAuthProvider>
+  <AppContext credential={credentials}>{Story}</AppContext>
 );
+
+type AppContextProps = {
+  children: string | JSX.Element | JSX.Element[];
+  credential: any;
+};
+
+export const AppContext = ({ credential, children }: AppContextProps) => {
+  return (
+    <GoogleOAuthProvider clientId={credential.web.client_id}>
+      <MyPhoneContext>{children}</MyPhoneContext>
+    </GoogleOAuthProvider>
+  );
+};
 
 const MyPhoneContext = styled.div`
   background-color: black;
