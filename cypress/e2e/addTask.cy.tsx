13b2fad769f87919b1ebe3c60f6b8cdeb7260752
Agustin Allamano Costa <allamanocostaagustin@gmail.com>
2024-02-07 17:13:09 +1100

    test: 💍 increase wait time in e2e test

--- cypress/e2e/addTask.cy.tsx
+++ cypress/e2e/addTask.cy.tsx
@@ -59,6 +59,7 @@ describe('Get The Things Done Task', () => {
     cy.get('[data-cy="task-add-button-input"]').type('{enter}');
     cy.wait(3000);
     cy.reload();
+    cy.wait(1000);
     cy.get('[data-cy="task-some task to do 1"]').should(
       'have.text',
       taskContent,
